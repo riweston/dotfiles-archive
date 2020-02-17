5b5249ad551c4ed8ab833999449114a983ae5992
Richard Weston <richard.weston@cloudreach.com>
2020-02-17 13:47:01 +0000

    rationalising config structure

--- dot_zshrc.d/dot_fzf.zsh
+++ dot_zshrc.d/dot_fzf.zsh
@@ -11,3 +11,9 @@ fi
 # Key bindings
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# Config option exports
+# ---------------------
+export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(code {})+abort'"
+export FZF_CTRL_T_OPTS="--height 80% --preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
--- dot_zshrc.d/source.zsh
+++ dot_zshrc.d/source.zsh
@@ -1,4 +1,19 @@
-# Source chtf
+# PowerLevel 10k config
+[[ ! -f ~/.zshrc.d/.p10k.zsh ]] || source ~/.zshrc.d/.p10k.zsh
+
+# PowerLevel 10k Instant Prompt
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Antibody Plugins
+source <(antibody init)
+antibody bundle <$HOME/.zshrc.d/plugins.zsh
+
+# fzf (Fuzzyfind)
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# chtf (Change Terraform)
 if [[ -f /usr/local/share/chtf/chtf.sh ]]; then
     source "/usr/local/share/chtf/chtf.sh"
 fi
