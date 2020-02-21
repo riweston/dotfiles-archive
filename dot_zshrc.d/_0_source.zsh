6e4b32e74b12c8ea6148abed203bb386bafbfbeb
Richard Weston <richard.weston@cloudreach.com>
2020-02-21 10:22:33 +0000

    removed some duplicate entries

--- dot_zshrc.d/_0_source.zsh
+++ dot_zshrc.d/_0_source.zsh
@@ -1,6 +1,3 @@
-# PowerLevel 10k config
-[[ ! -f ~/.zshrc.d/.p10k.zsh ]] || source ~/.zshrc.d/.p10k.zsh
-
 # PowerLevel 10k Instant Prompt
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
     source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
@@ -9,9 +6,6 @@ fi
 # Antibody Plugins
 source <(antibody init)
 antibody bundle <$HOME/.zshrc.d/plugins.zsh
-
-# fzf (Fuzzyfind)
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # chtf (Change Terraform)
 if [[ -f /usr/local/share/chtf/chtf.sh ]]; then
--- dot_zshrc.d/path.zsh
+++ dot_zshrc.d/path.zsh
@@ -4,5 +4,6 @@ path+=$HOME/gems/bin
 path+=/usr/local/opt/fzf/bin
 path+=/usr/local/sbin
 path+=/usr/local/opt/ncurses/bin
+path+=$HOME/Library/Python/2.7/bin/
 
 export path
